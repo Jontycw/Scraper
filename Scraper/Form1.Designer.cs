371102f2388c08a4f3245c6adcf70a974da26688
jontycw <jontycw@gmail.com>
2018-03-15 20:51:01 +0200

    Initial Scraper setup

---
+++ Scraper/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Scraper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RunBtn = new System.Windows.Forms.Button();
+            this.UrlTbx = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.OutputTa = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // RunBtn
+            // 
+            this.RunBtn.Location = new System.Drawing.Point(355, 183);
+            this.RunBtn.Name = "RunBtn";
+            this.RunBtn.Size = new System.Drawing.Size(75, 23);
+            this.RunBtn.TabIndex = 0;
+            this.RunBtn.Text = "Run";
+            this.RunBtn.UseVisualStyleBackColor = true;
+            this.RunBtn.Click += new System.EventHandler(this.RunBtn_Click);
+            // 
+            // UrlTbx
+            // 
+            this.UrlTbx.Location = new System.Drawing.Point(53, 15);
+            this.UrlTbx.Name = "UrlTbx";
+            this.UrlTbx.Size = new System.Drawing.Size(377, 20);
+            this.UrlTbx.TabIndex = 1;
+
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "URL";
+            // 
+            // OutputTa
+            // 
+            this.OutputTa.Location = new System.Drawing.Point(12, 50);
+            this.OutputTa.Name = "OutputTa";
+            this.OutputTa.Size = new System.Drawing.Size(418, 127);
+            this.OutputTa.TabIndex = 3;
+            this.OutputTa.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 218);
+            this.Controls.Add(this.OutputTa);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UrlTbx);
+            this.Controls.Add(this.RunBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RunBtn;
+        private System.Windows.Forms.TextBox UrlTbx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox OutputTa;
+    }
+}
+
---
+++ Scraper/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Net;
+using System.IO;
+using System.Net.Http;
+
+namespace Scraper
+{
+    public partial class Form1 : Form
+    {
+        public Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        public void InitTimer()
+        {
+            timer = new Timer();
+            timer.Tick += new EventHandler(RunBtn_Click);
+            timer.Interval = 1000;
+            timer.Start();
+        }
+
+        private void RunBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                InitTimer();
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36");
+
+                //using (StreamReader sr = new StreamReader("input.txt"))
+                //{
+                //    while (!sr.EndOfStream)
+                //    {
+                //        var html = client.GetStringAsync(sr.ReadLine()).Result;
+                var html = client.GetStringAsync(UrlTbx.Text).Result;
+                HtmlAgilityPack.HtmlDocument htmlDocument = new HtmlAgilityPack.HtmlDocument();
+                htmlDocument.LoadHtml(html);
+
+                HtmlNode productDataNode = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='product-data left']/div[1]/div[1]");
+                string title = productDataNode.SelectSingleNode("//h1").InnerText.Trim();
+
+                HtmlNode amountNode = productDataNode.SelectSingleNode("//span[@class='amount']");
+                if (amountNode == null)
+                    amountNode = htmlDocument.DocumentNode.SelectSingleNode("//div[@id='buybox']").SelectSingleNode("//span[@class='amount']");
+                string trimmed = amountNode.InnerText.Replace(",", "");
+                double price = double.Parse(trimmed);
+                OutputTa.Text = "Date: " + DateTime.Now + "\r\n" + title + "\r\nR " + price + "";
+               // File.AppendAllText("output.txt", $"{DateTime.Now} : {title} R{price}");
+                //    }
+                //}
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
